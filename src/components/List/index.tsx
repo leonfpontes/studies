8e466cca8ae7d8c251f361e2eec76df87a1f6c3a
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-22 17:09:48 -0300

    list component

---
+++ src/components/List/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function List() {
+    const tasks=[
+        {
+            task: 'React',
+            time: '02:00:00'
+        },
+        {
+            task: 'Javascript',
+            time: '02:30:00'
+        },
+        {
+            task: 'Typescript',
+            time: '03:00:00'
+        }
+    ]
+    return(
+        <aside>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tasks.map((item, index) => (
+                    <li key={index}>
+                        <h3>
+                            {item.task}
+                        </h3>
+                        <span>
+                            {item.time}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List
