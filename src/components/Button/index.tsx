704632ee12d835b628087392b89ff662f70c3dac
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-24 09:27:29 -0300

    trocando o class component Button para function interface

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import style from './Button.module.scss';
 
-class Button extends React.Component <{text: string, type?: "button" | "submit" | "reset" | undefined, onClick?: () => void}> {
+interface Props {
+    text: string, type?: "button" | "submit" | "reset" | undefined, 
+    onClick?: () => void
+    children?: React.ReactNode
+}
+
+function Button({onClick, type, children } : Props) {
+    return(
+        <button onClick={onClick} type={type} className={style.botao}>
+            {children}
+        </button>
+    )
+}
+
+/* class Button1 extends React.Component <{text: string, type?: "button" | "submit" | "reset" | undefined, onClick?: () => void}> {
     render() {
         const { text, type = "button", onClick} = this.props;
         return(
@@ -10,6 +24,6 @@ class Button extends React.Component <{text: string, type?: "button" | "submit" 
             </button>
         )
     }
-}
+} */
 
 export default Button
